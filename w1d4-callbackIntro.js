aa4fce9b807eba9aa785767c82c896a493b5b77f
Jeff <jeff.tang@gmail.com>
2018-05-10 16:44:31 +0000

    improved original index w for loop

--- w1d4-callbackIntro.js
+++ w1d4-callbackIntro.js
@@ -12,24 +12,26 @@ function runner(f) {
 runner(myFn);
  */
 
- // The second argument/parameter is expected to be a function
- let indexlocation = 0
+// The second argument/parameter is expected to be a function
+let indexlocation = 0
+
 function findWaldo(arr, found) {
-/*   for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      foundlocation = i
-      found();   // execute callback
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === "Waldo") {
+        // foundlocation = i
+        found(i);   // execute callback
+      }
     }
-  } */
-  arr.forEach(function(element) {
-    if (element === "Waldo"){
+/*   arr.forEach(function (element) {
+    if (element === "Waldo") {
       found()
-    } indexlocation += 1
-  })
+    }
+    indexlocation += 1
+  }) */
 }
 
-function actionWhenFound() {
-  console.log("Found him at index "  + indexlocation);
+function actionWhenFound(index) {
+  console.log("Found him at index " + index);
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
