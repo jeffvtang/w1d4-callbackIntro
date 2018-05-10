3c182568a65c6f98721a338fae9ebadc08f49e02
Jeff <jeff.tang@gmail.com>
2018-05-10 15:27:15 +0000

    replace for loop with a forEach (index log not working yet)

--- w1d4-callbackIntro.js
+++ w1d4-callbackIntro.js
@@ -14,16 +14,21 @@ runner(myFn);
 
  // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
+/*   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
       foundlocation = i
       found();   // execute callback
     }
-  }
+  } */
+  arr.forEach(function(element) {
+    if (element === "Waldo"){
+      found()
+    }
+  })
 }
 
 function actionWhenFound() {
-  console.log("Found him at index " + foundlocation);
+  console.log("Found him");
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
