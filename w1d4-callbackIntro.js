3be2c3aba86036e53297b2cfbd526c3879827abb
Jeff <jeff.tang@gmail.com>
2018-05-10 15:13:11 +0000

    Print the index of Waldo when found

---
+++ w1d4-callbackIntro.js
@@ -0,0 +1,29 @@
+/* var myFn = function() {
+  console.log("I am function.");
+}
+
+myFn.someAttribute = 42;
+console.log(myFn.someAttribute);
+
+function runner(f) {
+  f();
+}
+
+runner(myFn);
+ */
+
+ // The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      foundlocation = i
+      found();   // execute callback
+    }
+  }
+}
+
+function actionWhenFound() {
+  console.log("Found him at index " + foundlocation);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
