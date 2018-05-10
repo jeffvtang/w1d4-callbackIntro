b2cbbd49355ea3e1a86562698a341e8cda7cb08f
Jeff <jeff.tang@gmail.com>
2018-05-10 15:31:28 +0000

    added counter method for index counter

--- w1d4-callbackIntro.js
+++ w1d4-callbackIntro.js
@@ -13,6 +13,7 @@ runner(myFn);
  */
 
  // The second argument/parameter is expected to be a function
+ let indexlocation = 0
 function findWaldo(arr, found) {
 /*   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
@@ -23,12 +24,12 @@ function findWaldo(arr, found) {
   arr.forEach(function(element) {
     if (element === "Waldo"){
       found()
-    }
+    } indexlocation += 1
   })
 }
 
 function actionWhenFound() {
-  console.log("Found him");
+  console.log("Found him at index "  + indexlocation);
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
